ca898ef173899166ec5ef796e1210d25fa27551f
Emilorzz <emil69h@gmail.com>
2023-11-25 08:37:01 +0100

    added benchmarks for syntax and determinism

--- benches/clock_reduction_bench.rs
+++ benches/clock_reduction_bench.rs
@@ -4,13 +4,16 @@ use std::time::Duration;
 
 mod bench_helper;
 use reveaal::extract_system_rep::create_executable_query;
+use reveaal::model_objects::expressions::QueryExpression::Prune;
 use reveaal::parse_queries::parse_to_query;
 
 // const QUERY: &str = "refinement: (((((Adm2 && HalfAdm1 && HalfAdm2) || Machine || Researcher) && ((Adm2 && HalfAdm1) || Machine || Researcher) && ((Adm2 && HalfAdm2) || Machine || Researcher) && ((HalfAdm1 && HalfAdm2) || Machine || Researcher) && (Adm2 || Machine || Researcher)) // (Adm2 && HalfAdm1 && HalfAdm2)) // Researcher) <= (((((Adm2 && HalfAdm1 && HalfAdm2) || Machine || Researcher) && ((Adm2 && HalfAdm1) || Machine || Researcher) && ((Adm2 && HalfAdm2) || Machine || Researcher) && ((HalfAdm1 && HalfAdm2) || Machine || Researcher) && (Adm2 || Machine || Researcher)) // (Adm2 && HalfAdm1 && HalfAdm2)) // Researcher)";
 const REFINEMENT_QUERY: &str = "refinement: Researcher <= Spec // Administration // Machine";
 const REACHABILITY_QUERY: &str = "reachability: Machine || Researcher @ Machine.L5 && Researcher.L6 -> Machine.L4 && Researcher.L9";
-const CONSISTENCY_QUERY: &str = "consistency: Machine";
-const GETCOMPONENT_QUERY: &str = "get-component: Adm2 || Machine save-as test";
+const CONSISTENCY_QUERY: &str = "consistency: Machine || Researcher";
+const SYNTAX_QUERY: &str = "syntax: Researcher";
+const GETCOMPONENT_QUERY: &str = "get-component: Adm2 || Machine save-as get_component_test";
+const DETERMINISM_QUERY: &str = "determinism: Researcher && Machine";
 
 /// This bench runs `REFINEMENT QUERY` with and without clock reduction such that you can compare the results. It also runs other queries
 fn bench_clock_reduction(c: &mut Criterion) {
@@ -33,9 +36,17 @@ fn bench_clock_reduction(c: &mut Criterion) {
         loader.get_settings_mut().disable_clock_reduction = false;
         b.iter(|| clock_reduction_helper(&mut loader, CONSISTENCY_QUERY));
     });
+    group.bench_function("Syntax check - With reduction", |b| {
+        loader.get_settings_mut().disable_clock_reduction = false;
+        b.iter(|| clock_reduction_helper(&mut loader, SYNTAX_QUERY));
+    });
     group.bench_function("GetComponent check - With reduction", |b| {
         loader.get_settings_mut().disable_clock_reduction = false;
         b.iter(|| clock_reduction_helper(&mut loader, GETCOMPONENT_QUERY));
+    });
+    group.bench_function("Determinism check - With reduction", |b| {
+        loader.get_settings_mut().disable_clock_reduction = false;
+        b.iter(|| clock_reduction_helper(&mut loader, DETERMINISM_QUERY));
     });
 
     group.finish();
@@ -50,7 +61,7 @@ fn clock_reduction_helper(loader: &mut Box<dyn ComponentLoader>, input: &str) {
 
 criterion_group! {
     name = clock_reduction_bench;
-    config = Criterion::default().sample_size(10).warm_up_time(Duration::new(10,0));
+    config = Criterion::default().sample_size(10);
     targets = bench_clock_reduction
 }
 criterion_main!(clock_reduction_bench);
